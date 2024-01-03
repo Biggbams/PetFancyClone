0d193a889d3db79d731b8264952d2347ab650252
showole sheriff <showolesheriff7@gmail.com>
2024-01-03 03:00:46 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navandhero from "./navandhero";
+const App=()=>{
+    return(
+       <div style={
+        {
+            margin:0
+        }
+       }>
+        <Navandhero/>
+       </div> 
+    )
+}
+export default App;
